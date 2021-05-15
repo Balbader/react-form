a7b60210c07c8a65ff1a1b794d4d9e93fb57537f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-15 21:58:23 +0200

    update

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             job: 'Bartender',
           },
         ]
-
+    
         return (
           <div className="container">
             <Table characterData={characters} />
