2178e9db028aae14a31e0216c048f625b865808e
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-15 21:52:47 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import Table from './Table';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <Table />
-      </div>
-    )
-  }
+    render() {
+        const characters = [
+          {
+            name: 'Charlie',
+            job: 'Janitor',
+          },
+          {
+            name: 'Mac',
+            job: 'Bouncer',
+          },
+          {
+            name: 'Dee',
+            job: 'Aspring actress',
+          },
+          {
+            name: 'Dennis',
+            job: 'Bartender',
+          },
+        ]
+
+        return (
+          <div className="container">
+            <Table characterData={characters} />
+          </div>
+        )
+      }
 }
 
 export default App;
