e96407f0385f03b7671bbc4e5f8840ed34027f7c
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-17 12:20:12 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ class App extends Component {
     state = {
         characters: []
     };
+
+    removeCharacter = (index) => {
+        const {characters} = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+    }
+
+    render() {
+        const {characters} = this.state;
+
+        return (
+            <div className="container">
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
+            </div>
+        )
+    }
 }
 
 export default App;
