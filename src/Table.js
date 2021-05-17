4ce1bcbee8325e4fe4364d3254fef34f8909c818
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-17 12:31:24 +0200

    update

--- src/Table.js
+++ src/Table.js
@@ -17,23 +17,24 @@ const TableBody = (props) => {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
-        )
-    })
+        );
+    });
     return <tbody>{rows}</tbody>
 }
 
+const Table = (props) => {
+    const { characterData, removeCharacter} = props
 
-class Table extends Component {
-    render() {
-        const { characterData } = this.props;
-        return (
-            <table>
-                <TableHeader />
-                <TableBody characterData={characterData} />
-            </table>
-        )
-    }
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+        </table>
+    );
 }
 
 export default Table;
